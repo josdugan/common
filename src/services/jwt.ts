c42c081aba67c61c01b7cfd751cd85956eacbf0c
josdugan <josdugan@gmail.com>
2021-11-15 21:26:24 -0500

    Migrate common modules from auth to common

---
+++ src/services/jwt.ts
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { UserPayload } from '..';
+
+class JWTService {
+  static generate(user: UserPayload) {
+    return jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+  }
+
+  static verify(userJwt: string) {
+    return jwt.verify(userJwt, process.env.JWT_KEY!);
+  }
+}
+
+export { JWTService };
